381cb2aafbb2c33ee4baaece99bc0c57ae903006
JunHoPark <pjh374968@gmail.com>
2019-03-26 11:21:30 +0900

    refactor: Refactor GameHelper class
    메소드의 역할에 부합하기 위해 boolean 메소드의 출력 기능을 삭제, 오직 판별만 하게 끔 변경
    switch 문으로의 개선
    예외 처리 직관적으로 개선
    기타 변수명 개선
    시작 주석 추가

--- src/main/java/com/junho/javabaseball/GameHelper.java
+++ src/main/java/com/junho/javabaseball/GameHelper.java
@@ -1,69 +1,82 @@
+/*
+ * Class:   GameHelper
+ *
+ * Version: 1.0.0
+ *
+ * Date:    2019-03-26
+ *
+ * Author:  Jun Ho Park
+ *
+ */
+
 package com.junho.javabaseball;
 
 import com.junho.javabaseball.util.BaseBallUtil;
 
 import java.util.Scanner;
 
-/**
- * @author junho.park
- */
 public class GameHelper {
-    private static final int RESTART = 1;
-    private static final int EXIT = 2;
+    private static final int RESTART_CODE = 1;
+    private static final int EXIT_CODE = 2;
+    private static final int BASEBALL_NUM = 3;      // 야구 게임 3자리 수 기준
 
-    public static void play(Scanner sc, int[] generateNumbers) {
+    public static boolean play(Scanner sc, int[] generateNumbers) {
         while (true) {
             int[] guessNumbers = getUserInput(sc);
-
             String guessString = tryGuess(generateNumbers, guessNumbers);
+
             System.out.println(guessString);
 
-            if (GameHelper.endCondition(guessString)) {
-                break;
+            if (endCondition(guessString)) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+                return true;
             }
         }
     }
 
     public static boolean endCondition(String guessString) {
-        if (guessString.equals("3 스트라이크")) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
-            return true;
-        }
-        return false;
+        return guessString.equals("3 스트라이크");
     }
 
     public static int isContinue(Scanner sc) {
         while (true) {
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String input = sc.nextLine();
 
+            String input = sc.nextLine();
             if (!isStringNumberFormat(input)) {
                 continue;
             }
 
             int exitCode = Integer.parseInt(input);
-
-            if (exitCode == 1) {
-                return RESTART;
-            } else if (exitCode == 2) {
-                return EXIT;
-            } else {
-                System.out.println("1 또는 2를 입력하세요.");
+            switch (exitCode) {
+                case 1:
+                    return RESTART_CODE;
+                case 2:
+                    return EXIT_CODE;
+                default:
+                    System.out.println("1 또는 2를 입력하세요.");
             }
         }
     }
 
     private static int[] getUserInput(Scanner sc) {
-        int[] guessNumbers = new int[3];
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
-            String input = sc.nextLine();
 
-            if (!isStringNumberFormat(input) || !isStringLengthThree(input) || !isNumberInRange(input)) {
+            String input = sc.nextLine();
+            if (!isStringNumberFormat(input)) {
+                System.out.println("숫자 형식이 아닙니다.");
                 continue;
             }
-
-            return parseStringToIntegerArray(input, guessNumbers);
+            if (!isStringLengthEqualsToBaseBallNum(input)) {
+                System.out.println("길이가 3이 아닙니다.");
+                continue;
+            }
+            if(!isNumberInRange(input)) {
+                System.out.println("0은 입력할 수 없습니다.");
+                continue;
+            }
+            return parseStringToIntegerArray(input);
         }
     }
 
@@ -71,36 +84,32 @@ public class GameHelper {
         return BaseBallUtil.guessBaseBall(generateNumbers, guessNumbers);
     }
 
-    private static int[] parseStringToIntegerArray(String input, int[] guessNumbers) {
-        for (int i = 0; i < 3; i++) {
+    private static int[] parseStringToIntegerArray(String input) {
+        int givenLength = input.length();
+        int[] guessNumbers = new int[givenLength];
+        for (int i = 0; i < givenLength; i++) {
             guessNumbers[i] = Character.getNumericValue(input.charAt(i));
         }
         return guessNumbers;
     }
 
     private static boolean isNumberInRange(String input) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < BASEBALL_NUM; i++) {
             if (input.charAt(i) == '0') {
-                System.out.println("0은 입력할 수 없습니다.");
                 return false;
             }
         }
         return true;
     }
 
-    private static boolean isStringLengthThree(String input) {
-        if (input.length() != 3) {
-            System.out.println("길이가 3이 아닙니다.");
-            return false;
-        }
-        return true;
+    private static boolean isStringLengthEqualsToBaseBallNum(String input) {
+        return input.length() == BASEBALL_NUM;
     }
 
     private static boolean isStringNumberFormat(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            System.out.println("숫자 형식이 아닙니다.");
             return false;
         }
         return true;
