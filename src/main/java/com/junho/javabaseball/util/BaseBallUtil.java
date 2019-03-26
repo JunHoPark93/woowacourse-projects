408521fe2c6f1a61c0d80c37ff2a5a347e3cf840
JunHoPark <pjh374968@gmail.com>
2019-03-26 10:36:12 +0900

    refactor: Refactor BaseBallUtil class
    ThreadLocalRandom으로 변경 - 동시성 환경은 아니지만 추후 가능한 오버헤드를 줄이기 위해 메소드 변경
    Strike와 Ball을 판별하는 메소드 이름 직관적으로 변경
    시작 주석, 중요성 주석 추가

--- src/main/java/com/junho/javabaseball/util/BaseBallUtil.java
+++ src/main/java/com/junho/javabaseball/util/BaseBallUtil.java
@@ -1,52 +1,57 @@
+/*
+ * Class:   BaseBallUtil
+ *
+ * Version: 1.0.0
+ *
+ * Date:    2019-03-26
+ *
+ * Author:  Jun Ho Park
+ *
+ */
+
 package com.junho.javabaseball.util;
 
 import com.junho.javabaseball.model.BaseBallStatus;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
-/**
- * @author junho.park
- */
 public class BaseBallUtil {
-    private static final int BASEBALL_NUM = 3;
+    private static final int BASEBALL_NUM = 3;      // 야구 게임 3자리 수 기준
+    private static final int NUMBER_RANGE = 10;     // 야구 게임 숫자 범위
 
     public static int[] generateThreeDigitNumbers() {
-        boolean[] numberCheck = new boolean[10];
+        boolean[] numberCheck = new boolean[NUMBER_RANGE];
         int[] numbers = new int[BASEBALL_NUM];
-        Random random = new Random();
         int count = 0;
 
         while (count < BASEBALL_NUM) {
-            int number = random.nextInt(9) + 1;
-
+            // nextInt 함수는 0부터 시작하므로 인자로 1을 빼주고 결과값에 1을 더해야 1~9까지의 범위를 얻는다
+            int number = ThreadLocalRandom.current().nextInt(NUMBER_RANGE - 1) + 1;
             if (!numberCheck[number]) {
                 numbers[count] = number;
                 numberCheck[number] = true;
                 count++;
             }
         }
-
         return numbers;
     }
 
     public static String guessBaseBall(int[] generatedNumbers, int[] guessNumbers) {
         BaseBallStatus baseBallStatus = new BaseBallStatus(0,0);
-
         for (int i = 0; i < BASEBALL_NUM; i++) {
-            checkStrike(baseBallStatus, generatedNumbers[i], guessNumbers[i]);
-            checkBall(baseBallStatus, generatedNumbers[i], i, guessNumbers);
+            countStrike(baseBallStatus, generatedNumbers[i], guessNumbers[i]);
+            countBall(baseBallStatus, generatedNumbers[i], i, guessNumbers);
         }
-
         return baseBallStatus.toString();
     }
 
-    private static void checkStrike(BaseBallStatus baseBallStatus, int targetNumber, int guessNumber) {
+    private static void countStrike(BaseBallStatus baseBallStatus, int targetNumber, int guessNumber) {
         if (targetNumber == guessNumber) {
             baseBallStatus.setStrike(baseBallStatus.getStrike() + 1);
         }
     }
 
-    private static void checkBall(BaseBallStatus baseBallStatus, int targetNumber, int targetIndex, int[] guessNumbers) {
+    private static void countBall(BaseBallStatus baseBallStatus, int targetNumber, int targetIndex, int[] guessNumbers) {
         for (int j = 0; j < BASEBALL_NUM; j++) {
             if (j != targetIndex && guessNumbers[j] == targetNumber) {
                 baseBallStatus.setBall(baseBallStatus.getBall() + 1);
