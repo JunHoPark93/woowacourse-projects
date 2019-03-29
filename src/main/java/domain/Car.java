d0799c3a71de677492f9c591d8fbfbd9933bb23e
포비 <pobi@woowahan.com>
2019-03-29 11:47:06 +0900

    add Car class

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    // 추가 기능 구현
+}
