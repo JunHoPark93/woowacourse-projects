1e44dcf3bdc41e94cb688f56160114a2ae96c951
JunHoPark <pjh374968@gmail.com>
2019-04-08 20:18:42 +0900

    refactor: GameValidator 클래스 리팩토링
    한 함수의 기능은 오직 판별기능만 하게 끔 변경 (로그 출력 행위는 함수를 부른 쪽에서 작성해 주기로)
    테스트코드 수정

--- src/main/java/com/junhopark/javalotto/util/GameValidator.java
+++ src/main/java/com/junhopark/javalotto/util/GameValidator.java
@@ -11,7 +11,9 @@
 
 package com.junhopark.javalotto.util;
 
-import java.util.Arrays;
+import com.junhopark.javalotto.domain.Lotto;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class GameValidator {
@@ -20,102 +22,55 @@ public class GameValidator {
     private static final int MAX_LOTTO_NUM = 45;
     private static final int LOTTO_NUM = 6;
 
-    public static boolean isPurchaseMoneyInputValid(String input) {
-        if (!isParsableToLong(input)) {
-            return false;
-        }
-        Long money = Long.parseLong(input);
-        return isPositivePrice(money) && isOverMinimumLottoPrice(money);
+    public static boolean isPositivePrice(Long money) {
+        return money >= 0;
     }
 
-    public static boolean isWinningNumberInputValid(String input) {
-        List<String> numberList = Arrays.asList(input.split(","));
-
-        return isListAllParsableToInt(numberList)
-                && isValidLottoSize(numberList)
-                && isListAllValidLottoNumberRange(numberList);
+    public static boolean isOverMinimumLottoPrice(Long money) {
+        return money >= LOTTO_PRICE;
     }
 
-    public static boolean isBonusBallInputValid(List<Integer> lottoNumbers, String input) {
-        if (!isParsableToInt(input)) {
-            return false;
-        }
-
-        int bonusBall = Integer.parseInt(input);
-        if(!isValidLottoNumberRange(bonusBall)) {
-            return false;
-        }
-        return isLottoNumbersContainsBonusBall(lottoNumbers, bonusBall);
+    public static boolean isListValidLottoSize(List<Integer> numberList) {
+        return numberList.size() == LOTTO_NUM;
     }
 
-    private static boolean isPositivePrice(Long money) {
-        if (money < 0) {
-            System.out.println("돈은 음수가 될 수 없습니다.");
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean isOverMinimumLottoPrice(Long money) {
-        if (money < LOTTO_PRICE) {
-            System.out.println("구매할 돈이 부족이 부족합니다.");
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean isValidLottoSize(List<String> numberList) {
-        if (numberList.size() != LOTTO_NUM) {
-            System.out.println("로또는 6개의 숫자가 필요합니다.");
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean isParsableToLong(String input) {
+    public static boolean isParsableToLong(String input) {
         try {
             Long.parseLong(input);
         } catch (NumberFormatException e) {
-            System.out.println("64비트 정수입력 범위를 벗어났습니다.");
             return false;
         }
         return true;
     }
 
-    private static boolean isListAllParsableToInt(List<String> numberList) {
+    public static boolean isListAllParsableToInt(List<String> numberList) {
         return numberList.stream().allMatch(GameValidator::isParsableToInt);
     }
 
-    private static boolean isParsableToInt(String input) {
+    public static boolean isParsableToInt(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            System.out.println("32비트 정수입력 범위를 벗어났습니다.");
             return false;
         }
         return true;
     }
 
-    private static boolean isListAllValidLottoNumberRange(List<String> numberList) {
+    public static boolean isListAllValidLottoNumberRange(List<Integer> numberList) {
         return numberList.stream()
-                .map(Integer::valueOf)
                 .allMatch(GameValidator::isValidLottoNumberRange);
     }
 
-    private static boolean isValidLottoNumberRange(int number) {
-        if ((number < MIN_LOTTO_NUM) || (MAX_LOTTO_NUM < number)) {
-            System.out.println("로또 숫자의 범위를 벗어났습니다.");
-            return false;
-        }
-        return true;
+    public static boolean isValidLottoNumberRange(int number) {
+        return (number >= MIN_LOTTO_NUM) && (number <= MAX_LOTTO_NUM);
     }
 
-    private static boolean isLottoNumbersContainsBonusBall(List<Integer> lottoNumbers, int bonusBall) {
-        boolean isContain = lottoNumbers.stream().anyMatch(number -> number == bonusBall);
-        if (isContain) {
-            System.out.println("보너스 볼은 당첨 번호와 중복될 수 없습니다.");
-            return false;
-        }
-        return true;
+    public static boolean isLottoNumbersContainsBonusBall(Lotto lotto, int bonusBall) {
+        return lotto.getNumbers().stream().anyMatch(number -> number == bonusBall);
+    }
+
+    public static boolean isListDuplicateEachOther(List<Integer> list) {
+        HashSet<Integer> hashSet = new HashSet<>(list);
+        return hashSet.size() == list.size();
     }
 }
--- src/test/java/com/junhopark/javalotto/util/GameValidatorTest.java
+++ src/test/java/com/junhopark/javalotto/util/GameValidatorTest.java
@@ -11,8 +11,10 @@
 
 package com.junhopark.javalotto.util;
 
+import com.junhopark.javalotto.domain.Lotto;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,24 +22,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameValidatorTest {
     @Test
-    public void 구입금액_정상입력을_확인한다() {
-        // given
-        String input = "5000";
-
-        // when
-        boolean isValid = GameValidator.isPurchaseMoneyInputValid(input);
-
-        // then
-        assertThat(isValid).isTrue();
-    }
-
-    @Test
     public void 구입금액_문자_입력을_처리한다() {
         // given
         String input = "우아";
 
         // when
-        boolean isValid = GameValidator.isPurchaseMoneyInputValid(input);
+        boolean isValid = GameValidator.isParsableToLong(input);
 
         // then
         assertThat(isValid).isFalse();
@@ -46,10 +36,10 @@ public class GameValidatorTest {
     @Test
     public void 구입금액_로또한장가격_이하_입력을_처리한다() {
         // given
-        String input = "300";
+        long input = 300;
 
         // when
-        boolean isValid = GameValidator.isPurchaseMoneyInputValid(input);
+        boolean isValid = GameValidator.isOverMinimumLottoPrice(input);
 
         // then
         assertThat(isValid).isFalse();
@@ -58,10 +48,10 @@ public class GameValidatorTest {
     @Test
     public void 구입금액_음수_입력을_처리한다() {
         // given
-        String input = "-100";
+        long input = -100;
 
         // when
-        boolean isValid = GameValidator.isPurchaseMoneyInputValid(input);
+        boolean isValid = GameValidator.isPositivePrice(input);
 
         // then
         assertThat(isValid).isFalse();
@@ -73,7 +63,7 @@ public class GameValidatorTest {
         String input = "9223372036854775808";
 
         // when
-        boolean isValid = GameValidator.isPurchaseMoneyInputValid(input);
+        boolean isValid = GameValidator.isParsableToLong(input);
 
         // then
         assertThat(isValid).isFalse();
@@ -85,7 +75,7 @@ public class GameValidatorTest {
         String input = "!@#$%^&*()";
 
         // when
-        boolean isValid = GameValidator.isPurchaseMoneyInputValid(input);
+        boolean isValid = GameValidator.isParsableToLong(input);
 
         // then
         assertThat(isValid).isFalse();
@@ -94,10 +84,16 @@ public class GameValidatorTest {
     @Test
     public void 당첨번호입력_6개_입력을_확인한다() {
         // given
-        String input = "1,2,3,4,5,6";
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
 
         // when
-        boolean isValid = GameValidator.isWinningNumberInputValid(input);
+        boolean isValid = GameValidator.isListValidLottoSize(list);
 
         // then
         assertThat(isValid).isTrue();
@@ -106,10 +102,15 @@ public class GameValidatorTest {
     @Test
     public void 당첨번호입력_6개가_아닌_입력을_처리한다() {
         // given
-        String input = "1,2,3,4,5";
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
 
         // when
-        boolean isValid = GameValidator.isWinningNumberInputValid(input);
+        boolean isValid = GameValidator.isListValidLottoSize(list);
 
         // then
         assertThat(isValid).isFalse();
@@ -118,23 +119,34 @@ public class GameValidatorTest {
     @Test
     public void 당첨번호입력_문자입력을_처리한다() {
         // given
-        String input = "일,이,삼,사,오,육";
+        List<String> list = new ArrayList<>();
+        list.add("일");
+        list.add("이");
+        list.add("삼");
+        list.add("사");
+        list.add("오");
+        list.add("육");
 
         // when
-        boolean isValid = GameValidator.isWinningNumberInputValid(input);
+        boolean isValid = GameValidator.isListAllParsableToInt(list);
 
         // then
         assertThat(isValid).isFalse();
-
     }
 
     @Test
     public void 당첨번호입력_범위를_벗어난_양수입력을_처리한다() {
         // given
-        String input = "1,46,2,3,4,5";
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(46);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
 
         // when
-        boolean isValid = GameValidator.isWinningNumberInputValid(input);
+        boolean isValid = GameValidator.isListAllValidLottoNumberRange(list);
 
         // then
         assertThat(isValid).isFalse();
@@ -143,10 +155,16 @@ public class GameValidatorTest {
     @Test
     public void 당첨번호입력_범위를_벗어난_음수입력을_처리한다() {
         // given
-        String input = "1,30,2,3,25,-10";
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(-6);
 
         // when
-        boolean isValid = GameValidator.isWinningNumberInputValid(input);
+        boolean isValid = GameValidator.isListAllValidLottoNumberRange(list);
 
         // then
         assertThat(isValid).isFalse();
@@ -158,7 +176,7 @@ public class GameValidatorTest {
         String input = "\n";
 
         // when
-        boolean isValid = GameValidator.isWinningNumberInputValid(input);
+        boolean isValid = GameValidator.isParsableToLong(input);
 
         // then
         assertThat(isValid).isFalse();
@@ -168,23 +186,25 @@ public class GameValidatorTest {
     public void 보너스볼_입력_정상입력을_확인한다() {
         // given
         List<Integer> lottoNumber = Arrays.asList(1,2,3,4,5,6);
-        String bonusBall = "7";
+        Lotto lotto = new Lotto(lottoNumber);
+        int bonusBall = 7;
 
         // when
-        boolean isValid = GameValidator.isBonusBallInputValid(lottoNumber, bonusBall);
+        boolean isValid = GameValidator.isLottoNumbersContainsBonusBall(lotto, bonusBall);
 
         // then
-        assertThat(isValid).isTrue();
+        assertThat(isValid).isFalse();
     }
 
     @Test
     public void 보너스볼_입력_범위를_벗어난_입력을_처리한다() {
         // given
         List<Integer> lottoNumber = Arrays.asList(1,2,3,4,5,6);
-        String bonusBall = "100";
+        Lotto lotto = new Lotto(lottoNumber);
+        int bonusBall = 100;
 
         // when
-        boolean isValid = GameValidator.isBonusBallInputValid(lottoNumber, bonusBall);
+        boolean isValid = GameValidator.isLottoNumbersContainsBonusBall(lotto, bonusBall);
 
         // then
         assertThat(isValid).isFalse();
@@ -194,25 +214,13 @@ public class GameValidatorTest {
     public void 보너스볼_중복_입력을_처리한다() {
         // given
         List<Integer> lottoNumber = Arrays.asList(1,2,3,4,5,6);
-        String bonusBall = "6";
+        Lotto lotto = new Lotto(lottoNumber);
+        int bonusBall = 6;
 
         // when
-        boolean isValid = GameValidator.isBonusBallInputValid(lottoNumber, bonusBall);
+        boolean isValid = GameValidator.isLottoNumbersContainsBonusBall(lotto, bonusBall);
 
         // then
-        assertThat(isValid).isFalse();
-    }
-
-    @Test
-    public void 보너스볼_중복_단순개행을_처리한다() {
-        // given
-        List<Integer> lottoNumber = Arrays.asList(1,2,3,4,5,6);
-        String bonusBall = "\n";
-
-        // when
-        boolean isValid = GameValidator.isBonusBallInputValid(lottoNumber, bonusBall);
-
-        // then
-        assertThat(isValid).isFalse();
+        assertThat(isValid).isTrue();
     }
 }
