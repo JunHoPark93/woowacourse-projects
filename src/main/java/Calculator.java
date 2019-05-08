1e09418f7f31987cadd80d76bd348cb1eb94abf9
JunHoPark <pjh374968@gmail.com>
2019-05-08 14:41:13 +0900

    feat: 수업시간 calculator

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public int add(int i, int j) {
+        return i + j;
+    }
+
+    public int substract(int i, int j) {
+        return i - j;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void 덧셈() {
+        assertThat(calculator.add(2,3)).isEqualTo(5);
+    }
+
+    @Test
+    void 뺄셈() {
+        assertThat(calculator.substract(5,1)).isEqualTo(4);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("after each called");
+    }
+}
