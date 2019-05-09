f6acdc73a5ee8597727b65f0ad2372cce0e908fe
JunHoPark <pjh374968@gmail.com>
2019-05-09 12:16:47 +0900

    feat: Game 진행 클래스 정의
    게임 play 와 우승자 선출 기능 구현
    테스트 작성

---
+++ src/main/java/com/woowacourse/javaracingcar/Game.java
@@ -0,0 +1,69 @@
+package com.woowacourse.javaracingcar;
+
+import com.woowacourse.javaracingcar.domain.Car;
+import com.woowacourse.javaracingcar.dto.CarDto;
+import com.woowacourse.javaracingcar.util.interfaces.NumberGenerator;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+public class Game {
+    private static final int STOP_BOUND = 3;
+    private static final int MOVE_BOUND = 4;
+    private List<Car> cars;
+    private NumberGenerator numberGenerator;
+
+    public Game(NumberGenerator generator, List<Car> cars) {
+        numberGenerator = generator;
+        this.cars = cars;
+    }
+
+    public List<CarDto> play() {
+        // 게임 루프
+        for (Car c : cars)  {
+            c.moveForward(calculateMovingPosition(numberGenerator.generateNumber()));
+        }
+
+        return convertCarToCarDto(cars);
+    }
+
+    public List<CarDto> getWinners() {
+        // 우승자 선정
+        int max = calculateMaxPosition();
+        List<Car> winners = new ArrayList<>();
+
+        cars.stream().filter(car -> car.getPosition() == max)
+                .map(winners::add)
+                .collect(Collectors.toList());
+
+        return convertCarToCarDto(winners);
+    }
+
+    private List<CarDto> convertCarToCarDto(List<Car> cars) {
+        List<CarDto> list = new ArrayList<>();
+        for (Car c : cars) {
+            list.add(new CarDto(c.getName(), c.getPosition()));
+        }
+        return list;
+    }
+
+    private int calculateMovingPosition(int generatedNumber) {
+        if (generatedNumber >= MOVE_BOUND) {
+            return 1;
+        }
+        if (generatedNumber <= STOP_BOUND) {
+            return 0;
+        }
+        throw new IllegalArgumentException("올바르지 않은 인수: " + generatedNumber);
+    }
+
+    private int calculateMaxPosition() {
+        return cars.stream()
+                .max(Comparator.comparing(Car::getPosition))
+                .map(Car::getPosition)
+                .orElseThrow(NoSuchElementException::new);
+    }
+}
---
+++ src/test/java/com/woowacourse/javaracingcar/GameTest.java
@@ -0,0 +1,89 @@
+package com.woowacourse.javaracingcar;
+
+import com.woowacourse.javaracingcar.domain.Car;
+import com.woowacourse.javaracingcar.dto.CarDto;
+import com.woowacourse.javaracingcar.util.interfaces.NumberGenerator;
+import com.woowacourse.javaracingcar.testgenerator.TestNumberGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameTest {
+    private NumberGenerator numberGenerator;
+    private Game game;
+    private List<Car> cars;
+
+    @BeforeEach
+    void setUp() {
+        cars = new ArrayList<>();
+        cars.add(new Car("pobi"));
+        cars.add(new Car("crong"));
+        cars.add(new Car("honux"));
+    }
+
+    @Test
+    public void 자동차_전진() {
+        // given
+        int[] expected = {1,2,5};
+        numberGenerator = new TestNumberGenerator(expected);
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("pobi"));
+        game = new Game(numberGenerator, cars);
+        CarDto expectedWinnerCar = new CarDto("pobi", 1);
+
+        // when
+        for (int i = 0; i < expected.length; i++) {
+            game.play();
+        }
+
+        // then
+        assertThat(game.getWinners())
+                .hasSize(1)
+                .contains(expectedWinnerCar);
+    }
+
+    @Test
+    public void 정상_게임_종료_우승자_1명반환() {
+        // given
+        int[] expected = {1,2,3,1,2,4,4,5,6};
+        numberGenerator = new TestNumberGenerator(expected);
+        CarDto expectedWinnerCar = new CarDto("honux", 2);
+        game = new Game(numberGenerator, cars);
+
+        // when
+        for (int i = 0; i < 3; i++) {
+            game.play();
+        }
+
+        // then
+        assertThat(game.getWinners())
+                .hasSize(1)
+                .contains(expectedWinnerCar);
+    }
+
+    @Test
+    public void 정상_게임_종료_공동_우승자반환() {
+        // given
+        int[] expected ={4,1,4,5,2,5,6,4,6};
+        numberGenerator = new TestNumberGenerator(expected);
+        List<CarDto> expectedWinnerList = new ArrayList<>();
+        expectedWinnerList.add(new CarDto("pobi", 3));
+        expectedWinnerList.add(new CarDto("honux", 3));
+        game = new Game(numberGenerator, cars);
+
+        // when
+        for (int i = 0; i < 3; i++) {
+            game.play();
+        }
+        List<CarDto> actualWinnerList = game.getWinners();
+
+        // then
+        assertThat(actualWinnerList)
+                .hasSize(2)
+                .isEqualTo(expectedWinnerList);
+    }
+}
