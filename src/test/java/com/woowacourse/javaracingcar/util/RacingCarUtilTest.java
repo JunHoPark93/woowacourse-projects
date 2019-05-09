b75940a75c2b4be3ae4ea61c05e3a031098fd6d1
JunHoPark <pjh374968@gmail.com>
2019-05-09 15:19:48 +0900

    test: 유틸 클래스 테스트 순서 지정
    정상입력과 비정상 입력에 대한 테스트 순서를 지정해서 인텔리제이 왼쪽 하단 초록막대를 볼 때 정렬되서 보이게 한다

--- src/test/java/com/woowacourse/javaracingcar/util/RacingCarUtilTest.java
+++ src/test/java/com/woowacourse/javaracingcar/util/RacingCarUtilTest.java
@@ -1,12 +1,18 @@
 package com.woowacourse.javaracingcar.util;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RacingCarUtilTest {
+
+    @Order(1)
     @ParameterizedTest
     @ValueSource(strings = {"pobi,crong,honux",
                             "pobi,jay,jason",
@@ -16,6 +22,7 @@ class RacingCarUtilTest {
         assertThat(RacingCarUtil.isValidNameInput(RacingCarUtil.splitIntoNames(inputs))).isTrue();
     }
 
+    @Order(2)
     @ParameterizedTest
     @ValueSource(strings = {"pobi   ,   crong, honux ",
                             "   pobi, crong,honux",
@@ -24,29 +31,32 @@ class RacingCarUtilTest {
         assertThat(RacingCarUtil.isValidNameInput(RacingCarUtil.splitIntoNames(inputs))).isTrue();
     }
 
+    @Order(3)
     @ParameterizedTest
     @ValueSource(strings = {"pobi,crong,honuxxx",
             "pobiiiiiiiiii,crong,honux",
             "thelongestnameintheword"})
-    void 비정상_이름_입력_5자초과(String inputs) {
+    void 비정상_이름_입력_5자초과처리(String inputs) {
         assertThrows(IllegalArgumentException.class,
                 () -> RacingCarUtil.isValidNameInput(RacingCarUtil.splitIntoNames(inputs)));
     }
 
+    @Order(4)
     @ParameterizedTest
     @ValueSource(strings = {"p obi, crong,ho x",
                             " po bi, cro n",
                             "pobi,crong,hon x"})
-    void 비정상_공백처리(String inputs) {
+    void 비정상_이름_입력_공백처리(String inputs) {
         assertThrows(IllegalArgumentException.class,
                 () -> RacingCarUtil.isValidNameInput(RacingCarUtil.splitIntoNames(inputs)));
     }
 
+    @Order(5)
     @ParameterizedTest
     @ValueSource(strings = {"pobi,crong,crong",
                             "Jay, jay, Pobi",
                             "pobi,pobi,Bobi"})
-    void 비정상_중복된이름(String inputs) {
+    void 비정상_이름_입력_중복처리(String inputs) {
         assertThrows(IllegalArgumentException.class,
                 () -> RacingCarUtil.isValidNameInput(RacingCarUtil.splitIntoNames(inputs)));
     }
