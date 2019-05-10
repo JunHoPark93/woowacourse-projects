102397eabea49fd1e886f46828e78fc0e229d439
JunHoPark <pjh374968@gmail.com>
2019-05-10 09:55:55 +0900

    fix: 계산기 입력 공백 처리 추가
    앞 뒤 공백 입력시 정상 입력으로 처리하고, 공백만 들어왔을 시 예외로 한다

--- src/main/java/com/woowacourse/stringcalculator/StringCalculatorUtil.java
+++ src/main/java/com/woowacourse/stringcalculator/StringCalculatorUtil.java
@@ -5,22 +5,31 @@ import java.util.Queue;
 
 public class StringCalculatorUtil {
 
-
     public static StringCalculator parseCalculator(String input) {
+        // 공백 검사
+        input = removeSpaces(input);
+
         Queue<Character> operatorQueue = new LinkedList<>();
         Queue<Integer> numberQueue = new LinkedList<>();
         String[] tokens = input.split(" ");
-
         numberQueue.add(Integer.parseInt(tokens[0]));
-        int length = tokens.length;
 
-        for (int i = 1; i < length; i += 2) {
-            checkIfValidTokens(tokens, length, i);
+        int tokenLength = tokens.length;
+        for (int i = 1; i < tokenLength; i += 2) {
+            checkIfValidTokens(tokens, tokenLength, i);
+
             operatorQueue.add(tokens[i].charAt(0));
             numberQueue.add(Integer.parseInt(tokens[i+1]));
         }
 
         return new StringCalculator(operatorQueue, numberQueue);
+    }
+
+    private static String removeSpaces(String input) {
+        if (input.trim().length() == 0) {
+            throw new IllegalArgumentException("비어있는 식을 입력할 수 없습니다");
+        }
+        return input.trim();
     }
 
     private static void checkIfValidTokens(String[] tokens, int length, int i) {
@@ -29,21 +38,21 @@ public class StringCalculatorUtil {
         }
     }
 
-    private static boolean isNumber(String str) {
+    private static boolean isNumber(String mayBeNumber) {
         try {
-            Integer.parseInt(str);
+            Integer.parseInt(mayBeNumber);
             return true;
         } catch (NumberFormatException e) {
             return false;
         }
     }
 
-    private static boolean isOperator(String str) {
-        if (str.length() == 0) {
+    private static boolean isOperator(String maybeOperator) {
+        if (maybeOperator.length() == 0) {
             return false;
         }
-        char c = str.charAt(0);
 
+        char c = maybeOperator.charAt(0);
         return c == '+' || c == '-' || c == '*' || c == '/';
     }
 }
