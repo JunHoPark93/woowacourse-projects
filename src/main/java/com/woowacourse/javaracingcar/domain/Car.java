5a601db4b38a042691e50670e56c05e7ee9d02c4
JunHoPark <pjh374968@gmail.com>
2019-05-10 13:46:16 +0900

    refactor: Car 클래스 생성자 추가
    수업시간에 익힌 타 생성자 호출 방식 적용
    equals and hashcode 추가

--- src/main/java/com/woowacourse/javaracingcar/domain/Car.java
+++ src/main/java/com/woowacourse/javaracingcar/domain/Car.java
@@ -5,8 +5,12 @@ public class Car {
     private int position;
 
     public Car(String name) {
+        this(name, 0);
+    }
+
+    public Car(String name, int position) {
         this.name = name;
-        this.position = 0;
+        this.position = position;
     }
 
     public void moveForward(int position) {
@@ -19,5 +23,23 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (position != car.position) return false;
+        return name != null ? name.equals(car.name) : car.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + position;
+        return result;
     }
 }
