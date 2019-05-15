f4361577787b5aeac45ed46243cb72fc8d38a366
JunHoPark <pjh374968@gmail.com>
2019-05-15 10:36:03 +0900

    test: Add Natural Number test

---
+++ src/test/java/com/woowacourse/laddergame/util/NaturalNumberTest.java
@@ -0,0 +1,36 @@
+package com.woowacourse.laddergame.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class NaturalNumberTest {
+    @Test
+    void 자연수() {
+        int number = 3;
+
+        NaturalNumber naturalNumber = new NaturalNumber(number);
+
+        assertThat(naturalNumber.getNumber()).isEqualTo(3);
+    }
+
+    @Test
+    void 인덱스반환() {
+        int number = 3;
+
+        NaturalNumber naturalNumber = new NaturalNumber(number);
+
+        assertThat(naturalNumber.convertIndex()).isEqualTo(2);
+    }
+
+    @Test
+    void 음수() {
+        int number = -10;
+
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            NaturalNumber naturalNumber = new NaturalNumber(number);
+        }).withMessage("자연수가 아닙니다.");
+
+    }
+}
