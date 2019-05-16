49f214b6766fc9ccd5d9e358417309afec602a2b
JunHoPark <pjh374968@gmail.com>
2019-05-16 17:40:06 +0900

    refactor: Add equals and hashcode in Player

--- src/main/java/com/woowacourse/laddergame/domain/Player.java
+++ src/main/java/com/woowacourse/laddergame/domain/Player.java
@@ -27,6 +27,21 @@ public class Player {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        return name != null ? name.equals(player.name) : player.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
+    @Override
     public String toString() {
         return String.format("%6.6s", name);
     }
