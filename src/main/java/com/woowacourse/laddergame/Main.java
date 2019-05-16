c3c787dfb05a793c0d73bc4def1d29dab35e19b0
JunHoPark <pjh374968@gmail.com>
2019-05-16 12:25:47 +0900

    feat: Add initial Main class

---
+++ src/main/java/com/woowacourse/laddergame/Main.java
@@ -0,0 +1,29 @@
+package com.woowacourse.laddergame;
+
+import com.woowacourse.laddergame.domain.vo.LadderStatusVO;
+import com.woowacourse.laddergame.domain.vo.LadderVO;
+import com.woowacourse.laddergame.service.LadderGameService;
+import com.woowacourse.laddergame.view.InputView;
+import com.woowacourse.laddergame.view.OutputView;
+
+public class Main {
+    public static void main(String[] args) {
+        // view 호출
+        LadderVO ladderVO = new LadderVO();
+        InputView.inputPlayerNames(ladderVO);
+        InputView.inputHeight(ladderVO);
+
+        // GameService 인자로 넘긴다 - 사다리 vo 반환
+        LadderGameService ladderGameService = new LadderGameService();
+        ladderGameService.drawLadder(ladderVO);
+
+        LadderStatusVO ladderStatusVO = ladderGameService.getInitialLadder();
+        OutputView.printLadderStatus(ladderStatusVO);
+
+
+
+        // TODO Level 2
+        // while (player 결과 입력)
+        // ladderGameService.getResult(playerName);
+    }
+}
