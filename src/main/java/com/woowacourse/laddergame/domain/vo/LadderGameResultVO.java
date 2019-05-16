b9bed6732772286b50884b101df8971f50201969
JunHoPark <pjh374968@gmail.com>
2019-05-16 17:40:48 +0900

    feat: Add LadderGameResultVO class

---
+++ src/main/java/com/woowacourse/laddergame/domain/vo/LadderGameResultVO.java
@@ -0,0 +1,22 @@
+package com.woowacourse.laddergame.domain.vo;
+
+public class LadderGameResultVO {
+    private String playerName;
+    private String result;
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
