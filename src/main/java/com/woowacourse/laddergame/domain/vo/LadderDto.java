cdd46ee8d0ff676c775224c37a66175c5ac4fcea
JunHoPark <pjh374968@gmail.com>
2019-05-17 16:55:04 +0900

    refactor: Separate methods in LadderDto

--- src/main/java/com/woowacourse/laddergame/domain/vo/LadderDto.java
+++ src/main/java/com/woowacourse/laddergame/domain/vo/LadderDto.java
@@ -28,44 +28,43 @@ public class LadderDto {
         this.names = names;
     }
 
-    // TODO seperate
     private void checkPlayerNames(String names) {
-        if (names == null) {
-            throw new IllegalArgumentException("Null 은 입력할 수 없습니다");
-        }
-
-        if (names.contains(ILLEGAL_NAME)) {
-            throw new IllegalArgumentException("all은 player 이름으로 입력할 수 없습니다");
-        }
-
-        Matcher matcher = TOTAL_PLAYER_NAMES_PATTERN.matcher(names);
-        if (!matcher.find()) {
-            throw new IllegalArgumentException("Player 이름들이 잘못되었습니다");
-        }
+        checkNameIfNull(names);
+        checkNameIfIllegal(names);
+        checkNameMatchPattern(names);
 
         // Result가 들어와 있는상태에서 플레이어 이름을 다시 초기화 할 때
         if (!isSameLengthBetweenResultAndNames(getNamesCount(names))) {
             throw new IllegalArgumentException("Result 개수와 맞지 않습니다");
         }
+        checkDuplicateNames(names);
+    }
 
-        if (!containsDuplicateNames(names)) {
+    private void checkDuplicateNames(String names) {
+        List<String> nameTokens = Arrays.asList(names.split(","));
+        Set<String> set = new HashSet<>(nameTokens);
+        if (set.size() != nameTokens.size()) {
             throw new IllegalArgumentException("중복된 이름을 입력할 수 없습니다");
         }
     }
 
-    private boolean containsDuplicateNames(String names) {
-        List<String> nameTokens = Arrays.asList(names.split(","));
-        Set<String> set = new HashSet<>(nameTokens);
-        return set.size() == nameTokens.size();
+    private void checkNameMatchPattern(String names) {
+        Matcher matcher = TOTAL_PLAYER_NAMES_PATTERN.matcher(names);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Player 이름들이 잘못되었습니다");
+        }
     }
 
-    public int getHeight() {
-        return height;
+    private void checkNameIfIllegal(String names) {
+        if (names.contains(ILLEGAL_NAME)) {
+            throw new IllegalArgumentException("all은 player 이름으로 입력할 수 없습니다");
+        }
     }
 
-    public void setHeight(String height) {
-        checkHeight(height);
-        this.height = Integer.parseInt(height);
+    private void checkNameIfNull(String names) {
+        if (names == null) {
+            throw new IllegalArgumentException("Null 은 입력할 수 없습니다");
+        }
     }
 
     public void checkHeight(String height) {
@@ -79,28 +78,10 @@ public class LadderDto {
         }
     }
 
-    public String getResult() {
-        return result;
-    }
-
-    public void setResult(String result) {
-        checkResult(result);
-        this.result = result;
-    }
-
     public void checkResult(String result) {
-        if (result == null) {
-            throw new IllegalArgumentException("Null 은 입력할 수 없습니다");
-        }
-
-        if (names == null) {
-            throw new IllegalArgumentException("이름이 먼저 초기화되야 합니다");
-        }
-
-        Matcher matcher = TOTAL_RESULT_PATTERN.matcher(result);
-        if (!matcher.find()) {
-            throw new IllegalArgumentException("Result 값이 잘못 되었습니다");
-        }
+        checkResultisNull(result);
+        checkNamesInitialized();
+        checkResultMatchPattern(result);
 
         // Result 초기화 시 이름 개수와 비교
         if (!isSameLengthBetweenResultAndNames(getResultCount(result))) {
@@ -108,35 +89,69 @@ public class LadderDto {
         }
     }
 
+    private void checkResultMatchPattern(String result) {
+        Matcher matcher = TOTAL_RESULT_PATTERN.matcher(result);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Result 값이 잘못 되었습니다");
+        }
+    }
+
+    private void checkNamesInitialized() {
+        if (names == null) {
+            throw new IllegalArgumentException("이름이 먼저 초기화되야 합니다");
+        }
+    }
+
+    private void checkResultisNull(String result) {
+        if (result == null) {
+            throw new IllegalArgumentException("Null 은 입력할 수 없습니다");
+        }
+    }
+
     private int getResultCount(String result) {
-        int cnt = 0;
+        int count = 0;
 
         Matcher matcher = SINGLE_RESULT_PATTERN.matcher(result);
         while (matcher.find()) {
-            cnt++;
+            count++;
         }
-        return cnt;
+        return count;
     }
 
     private boolean isSameLengthBetweenResultAndNames(int count) {
         if (result != null && count != getResultCount(result)) {
             return false;
         }
-
         if (names != null && count != getNamesCount(names)) {
             return false;
         }
-
         return true;
     }
 
     private int getNamesCount(String result) {
         Matcher matcher = SINGLE_PLAYER_NAMES_PATTERN.matcher(result);
-
         int count = 0;
         while (matcher.find()) {
             count++;
         }
         return count;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        checkHeight(height);
+        this.height = Integer.parseInt(height);
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        checkResult(result);
+        this.result = result;
     }
 }
