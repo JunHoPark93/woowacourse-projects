aa7c554f54cb18a204331c95cf4046463d629641
JunHoPark <pjh374968@gmail.com>
2019-05-17 23:01:07 +0900

    refactor: Separate generating ladder method

--- src/main/java/com/woowacourse/laddergame/util/LadderGenerator.java
+++ src/main/java/com/woowacourse/laddergame/util/LadderGenerator.java
@@ -7,16 +7,25 @@ public class LadderGenerator {
         Ladder ladder = new Ladder(height, countOfPerson);
 
         // 처음 사다리 번호는 1번으로 칭한다
-        // TODO static 으로 하고 싶은데 어떻게 이 로직을 쪼갤지? height와 사람의 인덱스까지 필요하고 그려졌다면 다음 칸을 건너 뛰는 로직이라 고민
         for (int h = 1; h <= height.getNumber(); h++) {
-            for (int i = 1; i < countOfPerson.getNumber(); i++) {
-                if (generator.generate()) {
-                    ladder.putBridge(new NaturalNumber(h), new NaturalNumber(i));
-                    i++;
-                }
-            }
+            // 사람 수만 큼 (즉 Position 수 만큼) 반복한다
+            loopInPerson(countOfPerson, generator, ladder, h);
         }
 
         return ladder;
+    }
+
+    private static void loopInPerson(NaturalNumber countOfPerson, BooleanGenerator generator, Ladder ladder, int height) {
+        for (int i = 1; i < countOfPerson.getNumber(); i++) {
+            i = attemptToPutBridge(generator, ladder, height, i);
+        }
+    }
+
+    private static int attemptToPutBridge(BooleanGenerator generator, Ladder ladder, int height, int personIdx) {
+        if (generator.generate()) {
+            ladder.putBridge(new NaturalNumber(height), new NaturalNumber(personIdx));
+            personIdx++;
+        }
+        return personIdx;
     }
 }
