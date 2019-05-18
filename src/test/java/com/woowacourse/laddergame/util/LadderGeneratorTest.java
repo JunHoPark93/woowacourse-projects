68e8fe203fee9fe3e25a9c3008c7621ddefff123
JunHoPark <pjh374968@gmail.com>
2019-05-18 09:35:36 +0900

    refactor: Remove console test
    The process that confirming random generating ladder is not proper in test code.
    Though this case is insignificant, in the real world it can be an overhead when there are lots of them.

--- src/test/java/com/woowacourse/laddergame/util/LadderGeneratorTest.java
+++ src/test/java/com/woowacourse/laddergame/util/LadderGeneratorTest.java
@@ -1,6 +1,5 @@
 package com.woowacourse.laddergame.util;
 
-import com.woowacourse.laddergame.domain.Ladder;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,16 +21,5 @@ public class LadderGeneratorTest {
         BooleanGenerator generator = new TestBooleanGenerator(testData);
 
         assertThat(generator.generate()).isFalse();
-    }
-
-    @Test
-    void 실제_사다리_확인용_메서드() {
-        // 랜덤이 제대로 먹는지 콘솔에서 확인한다
-        BooleanGenerator generator = new RandomBooleanGenerator();
-        NaturalNumber height = new NaturalNumber(3);
-        NaturalNumber countOfPerson = new NaturalNumber(4);
-        Ladder ladder = LadderGenerator.generateLadder(height, countOfPerson, generator);
-
-        System.out.println(ladder.toString());
     }
 }
