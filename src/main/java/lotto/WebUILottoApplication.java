935c969131380c85d5633c246dbbeb398bf78122
포비 <pobi@woowahan.com>
2019-05-22 16:28:53 +0900

    initial commit

---
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+
+public class WebUILottoApplication {
+    public static void main(String[] args) {
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "index.html");
+        });
+    }
+
+    private static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
+}
