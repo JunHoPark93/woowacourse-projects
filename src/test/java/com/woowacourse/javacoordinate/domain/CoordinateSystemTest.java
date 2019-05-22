712a8f13f678eb036b1287756ac166a6472f4df1
JunHoPark <pjh374968@gmail.com>
2019-05-22 18:11:50 +0900

    refactor: Remove console test

--- src/test/java/com/woowacourse/javacoordinate/domain/CoordinateSystemTest.java
+++ src/test/java/com/woowacourse/javacoordinate/domain/CoordinateSystemTest.java
@@ -1,11 +1,8 @@
 package com.woowacourse.javacoordinate.domain;
 
-import com.woowacourse.javacoordinate.view.OutputView;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,31 +23,5 @@ public class CoordinateSystemTest {
 
         assertThat(coordinateSystem.isMarked(20,0)).isTrue();
         assertThat(coordinateSystem.isMarked(19,0)).isFalse();
-    }
-
-    @Test
-    void 라인테스트() {
-        Boolean[] line = new Boolean[25];
-        Boolean[] line2 = new Boolean[25];
-
-        for (int i = 0; i < line.length; i++) {
-            line[i] = false;
-            line2[i] = false;
-        }
-
-        line2[2] = true;
-
-        List<CoordinateLine> lines = new ArrayList<>();
-        for (int i = 0; i < 25; i++) {
-            if (i == 10) {
-                lines.add(new CoordinateLine(Arrays.asList(line2)));
-            }
-            lines.add(new CoordinateLine(Arrays.asList(line)));
-        }
-
-        CoordinateSystem coordinateSystem
-                = new CoordinateSystem(lines);
-
-        OutputView.printCoordinateSystem(coordinateSystem);
     }
 }
