7284f7f0389445ef3db533ad8e2cf12508fc4139
JunHoPark <pjh374968@gmail.com>
2019-05-27 14:05:02 +0900

    refactor: Remove if statement in getting Result

--- src/main/java/com/woowacourse/javacoordinate/util/FigureUtil.java
+++ src/main/java/com/woowacourse/javacoordinate/util/FigureUtil.java
@@ -1,20 +1,25 @@
 package com.woowacourse.javacoordinate.util;
 
-import com.woowacourse.javacoordinate.domain.*;
+import com.woowacourse.javacoordinate.domain.Figure;
+import com.woowacourse.javacoordinate.domain.Result;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 public class FigureUtil {
-    private static final int LENGTH_CAL_CODE = 1;
-    private static final int AREA_CAL_CODE = 2;
     private static final String LENGTH = "길이";
     private static final String AREA = "넓이";
 
+    private final static Map<Integer, Function<Figure, Result>> map = new HashMap<>();
+
+    static {
+        map.put(1, (Figure figure) -> new Result(figure.getName(), LENGTH, figure.calculateLength()));
+        map.put(2, (Figure figure) -> new Result(figure.getName(), AREA, figure.calculateArea()));
+    }
+
     public static Result createResult(Figure figure, int calculateCode) {
-        if (calculateCode == LENGTH_CAL_CODE) {
-            return new Result(figure.getName(), LENGTH, figure.calculateLength());
-        }
-        if (calculateCode == AREA_CAL_CODE) {
-            return new Result(figure.getName(), AREA, figure.calculateArea());
-        }
-        throw new IllegalArgumentException("잘못된 호출입니다");
+        Function<Figure, Result> figureResultFunction = map.get(calculateCode);
+        return figureResultFunction.apply(figure);
     }
 }
