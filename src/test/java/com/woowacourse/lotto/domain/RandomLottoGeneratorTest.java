30b3f945ef16dec9a78bcb924d53c6465abe6af0
JunHoPark <pjh374968@gmail.com>
2019-05-28 17:07:08 +0900

    test: Random lotto size test

---
+++ src/test/java/com/woowacourse/lotto/domain/RandomLottoGeneratorTest.java
@@ -0,0 +1,23 @@
+package com.woowacourse.lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RandomLottoGeneratorTest {
+    private static final int MIN_LOTTO_NUM = 1;
+    private static final int MAX_LOTTO_NUM = 45;
+
+    @Test
+    void 로또_범위_안의_랜덤_숫자_생성() {
+        LottoGenerator lottoGenerator = new RandomLottoGenerator();
+
+        for (int i = 0; i < 10000; i++) {
+            assertThat(validLottoRange(lottoGenerator.generateNumber())).isTrue();
+        }
+    }
+
+    private boolean validLottoRange(int generateNumber) {
+        return MIN_LOTTO_NUM <= generateNumber && generateNumber <= MAX_LOTTO_NUM;
+    }
+}
