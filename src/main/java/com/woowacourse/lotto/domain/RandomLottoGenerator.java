e92763a45b55cbe0a95fca1bb760872edadd2625
JunHoPark <pjh374968@gmail.com>
2019-05-28 17:07:40 +0900

    feat: Define random lotto generator implementing Lotto Generator

--- src/main/java/com/woowacourse/lotto/domain/RandomLottoGenerator.java
+++ src/main/java/com/woowacourse/lotto/domain/RandomLottoGenerator.java
@@ -1,8 +1,13 @@
 package com.woowacourse.lotto.domain;
 
+import java.util.Random;
+
 public class RandomLottoGenerator implements LottoGenerator {
+    private static final int MAX_LOTTO_NUM = 45;
+    private static final Random RANDOM = new Random();
+
     @Override
     public int generateNumber() {
-        return 0;
+        return RANDOM.nextInt(MAX_LOTTO_NUM - 1) + 1;
     }
 }
