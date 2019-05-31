6343651569cb7df8645040746e62190e957f6c30
포비 <pobi@woowahan.com>
2019-05-31 10:10:04 +0900

    initial commit

---
+++ src/main/java/chess/WebUIChessApplication.java
@@ -0,0 +1,22 @@
+package chess;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+
+public class WebUIChessApplication {
+    public static void main(String[] args) {
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "index.html");
+        });
+    }
+
+    private static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
+}
