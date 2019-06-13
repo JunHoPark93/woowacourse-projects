ad5fee141fba429dd0a0097552f8335b7307653f
JunHoPark <pjh374968@gmail.com>
2019-06-13 20:13:08 +0900

    fix: Print bonus number

--- src/main/java/com/woowacourse/lotto/domain/BonusNumber.java
+++ src/main/java/com/woowacourse/lotto/domain/BonusNumber.java
@@ -37,8 +37,6 @@ public class BonusNumber implements Number {
 
     @Override
     public String toString() {
-        return "BonusNumber{" +
-                "bonusNum=" + bonusNum +
-                '}';
+        return String.valueOf(bonusNum);
     }
 }
