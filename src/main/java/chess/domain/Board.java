7b55a8ae37a0d0b75cc8b032b3e8c2d749e7cbe5
JunHoPark <pjh374968@gmail.com>
2019-06-17 15:44:21 +0900

    feat: 보드 생성

---
+++ src/main/java/chess/domain/Board.java
@@ -0,0 +1,26 @@
+package chess.domain;
+
+import java.util.List;
+
+public class Board {
+    private final List<Square> board = SquareFactory.boards;
+
+    public int getSize() {
+        return board.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Board board1 = (Board) o;
+
+        return board != null ? board.equals(board1.board) : board1.board == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return board != null ? board.hashCode() : 0;
+    }
+}
---
+++ src/main/java/chess/domain/SquareFactory.java
@@ -0,0 +1,17 @@
+package chess.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SquareFactory {
+    public static final List<Square> boards;
+
+    static {
+        boards = new ArrayList<>();
+        for (XPosition xPosition : XPositionFactory.xPositions) {
+            for (YPosition yPosition : YPositionFactory.yPositions) {
+                boards.add(new Square(xPosition, yPosition));
+            }
+        }
+    }
+}
