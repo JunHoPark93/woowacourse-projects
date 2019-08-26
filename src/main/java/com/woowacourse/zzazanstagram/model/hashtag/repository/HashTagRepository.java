f3f1127132776e221ce298199bfe94e48d70de97
박효진 <hgenie14@gmail.com>
2019-08-26 18:14:21 +0900

    [#38] feat: 해시태그에서 사용되는 엔티티 연관관계 매핑

---
+++ src/main/java/com/woowacourse/zzazanstagram/model/hashtag/repository/HashTagRepository.java
@@ -0,0 +1,13 @@
+package com.woowacourse.zzazanstagram.model.hashtag.repository;
+
+import com.woowacourse.zzazanstagram.model.article.domain.Article;
+import com.woowacourse.zzazanstagram.model.hashtag.domain.HashTag;
+import com.woowacourse.zzazanstagram.model.hashtag.domain.TagKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface HashTagRepository extends JpaRepository<HashTag, Long> {
+    List<HashTag> findAllByArticle(Article article);
+    List<HashTag> findAllByTagKeyword(TagKeyword tagKeyword);
+}
