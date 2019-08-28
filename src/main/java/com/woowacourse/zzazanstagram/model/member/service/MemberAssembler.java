b7ad5dae2eecf2a0ea907fc9ef709d64e374aa37
starkim06 <starkim06@naver.com>
2019-08-28 16:02:53 +0900

    [#155] refactor: MemberFacade 클래스 제거
    마이페이지에 관련된 기능 ArticleController, Service로 이동

--- src/main/java/com/woowacourse/zzazanstagram/model/member/service/MemberAssembler.java
+++ src/main/java/com/woowacourse/zzazanstagram/model/member/service/MemberAssembler.java
@@ -1,6 +1,7 @@
 package com.woowacourse.zzazanstagram.model.member.service;
 
 import com.woowacourse.zzazanstagram.model.member.domain.Member;
+import com.woowacourse.zzazanstagram.model.member.dto.MemberMyPageResponse;
 import com.woowacourse.zzazanstagram.model.member.dto.MemberResponse;
 import com.woowacourse.zzazanstagram.model.member.dto.MemberSignUpRequest;
 
@@ -17,5 +18,18 @@ public class MemberAssembler {
                 .password(memberSignupRequest.getPassword())
                 .profile(memberSignupRequest.getProfile())
                 .build();
+    }
+
+    public static MemberMyPageResponse toMyMapgeResponse(Member member, long articleNumber, long followerNumber, long followeeNumber) {
+        return MemberMyPageResponse.builder.aMemberMyPageResponse()
+                .id(member.getId())
+                .profileImage(member.getProfileImageValue())
+                .nickName(member.getNickNameValue())
+                .name(member.getNameValue())
+                .articleNumber(articleNumber)
+                .followeeNumber(followeeNumber)
+                .followerNumber(followerNumber)
+                .build();
+
     }
 }
--- src/main/java/com/woowacourse/zzazanstagram/web/controller/member/MemberController.java
+++ src/main/java/com/woowacourse/zzazanstagram/web/controller/member/MemberController.java
@@ -1,28 +1,21 @@
 package com.woowacourse.zzazanstagram.web.controller.member;
 
-import com.woowacourse.zzazanstagram.model.member.dto.MemberMyPageResponse;
 import com.woowacourse.zzazanstagram.model.member.dto.MemberResponse;
 import com.woowacourse.zzazanstagram.model.member.dto.MemberSignUpRequest;
-import com.woowacourse.zzazanstagram.model.member.service.MemberFacadeService;
 import com.woowacourse.zzazanstagram.model.member.service.MemberService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 import java.util.Map;
 
-// TODO facade 제거
 @Controller
 public class MemberController {
-    private MemberFacadeService memberFacadeService;
     private final MemberService memberService;
     private final Map<String, MemberResponse> sessionMap;
-  
-    public MemberController(MemberFacadeService memberFacadeService, MemberService memberService, Map<String, MemberResponse> sessionMap) {
-        this.memberFacadeService = memberFacadeService;
+
+    public MemberController(MemberService memberService, Map<String, MemberResponse> sessionMap) {
         this.memberService = memberService;
         this.sessionMap = sessionMap;
     }
@@ -30,13 +23,6 @@ public class MemberController {
     @GetMapping("/signup")
     public String signUp() {
         return "signup";
-    }
-
-    @GetMapping("/members/{nickname}")
-    public String myPage(@PathVariable("nickname") String nickName, Model model) {
-        MemberMyPageResponse memberMyPageResponse = memberFacadeService.myPage(nickName);
-        model.addAttribute("member", memberMyPageResponse);
-        return "mypage";
     }
 
     @PostMapping("/members")
