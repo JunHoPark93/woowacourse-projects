5a0ff46835579375cc6d66eb20ee77131af4f6e2
starkim06 <starkim06@naver.com>
2019-08-28 15:25:27 +0900

    [#155] refactor: logout 핸들러 LoginController로 이동

--- src/main/java/com/woowacourse/zzazanstagram/web/controller/member/LoginController.java
+++ src/main/java/com/woowacourse/zzazanstagram/web/controller/member/LoginController.java
@@ -4,12 +4,12 @@ import com.woowacourse.zzazanstagram.model.member.MemberSession;
 import com.woowacourse.zzazanstagram.model.member.dto.MemberLoginRequest;
 import com.woowacourse.zzazanstagram.model.member.dto.MemberResponse;
 import com.woowacourse.zzazanstagram.model.member.service.LoginService;
+import com.woowacourse.zzazanstagram.web.SessionKeys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -19,7 +19,6 @@ import java.util.UUID;
 import static com.woowacourse.zzazanstagram.web.SessionKeys.MEMBER;
 
 @Controller
-@RequestMapping("/login")
 public class LoginController {
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
     private static final String TAG = "[LoginController]";
@@ -33,13 +32,13 @@ public class LoginController {
         this.sessionMap = sessionMap;
     }
 
-    @GetMapping
+    @GetMapping("/login")
     public String loginForm() {
         return "login";
     }
 
     // TODO endpoint 생성부분 서비스로 옮기기
-    @PostMapping
+    @PostMapping("/login")
     public String login(MemberLoginRequest memberLoginRequest, HttpSession httpSession, RedirectAttributes redirectAttributes) {
         MemberResponse memberResponse = loginService.find(memberLoginRequest);
         httpSession.setAttribute(MEMBER, new MemberSession(memberResponse.getId(), memberResponse.getName(), memberResponse.getEmail(), memberResponse.getNickName(), memberResponse.getProfileImage()));
@@ -58,5 +57,16 @@ public class LoginController {
 
     private String generateRandomEndPoint() {
         return UUID.randomUUID().toString();
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession httpSession) {
+        MemberSession memberSession = (MemberSession) httpSession.getAttribute(SessionKeys.MEMBER);
+        sessionMap.entrySet().stream()
+                .filter(entry -> entry.getValue().getNickName().equals(memberSession.getNickName()))
+                .map(Map.Entry::getKey)
+                .forEach(sessionMap::remove);
+        httpSession.removeAttribute(SessionKeys.MEMBER);
+        return "redirect:/login";
     }
 }
--- src/main/java/com/woowacourse/zzazanstagram/web/controller/member/MemberController.java
+++ src/main/java/com/woowacourse/zzazanstagram/web/controller/member/MemberController.java
@@ -1,20 +1,16 @@
 package com.woowacourse.zzazanstagram.web.controller.member;
 
 import com.woowacourse.zzazanstagram.model.member.dto.MemberMyPageResponse;
-import com.woowacourse.zzazanstagram.model.member.MemberSession;
 import com.woowacourse.zzazanstagram.model.member.dto.MemberResponse;
 import com.woowacourse.zzazanstagram.model.member.dto.MemberSignUpRequest;
 import com.woowacourse.zzazanstagram.model.member.service.MemberFacadeService;
 import com.woowacourse.zzazanstagram.model.member.service.MemberService;
-import com.woowacourse.zzazanstagram.web.SessionKeys;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.Map;
 
@@ -46,18 +42,6 @@ public class MemberController {
     @PostMapping("/members")
     public String saveMember(@Valid MemberSignUpRequest memberSignupRequest) {
         memberService.save(memberSignupRequest);
-        return "redirect:/login";
-    }
-
-    // TODO logincontroller 로 옮기기
-    @GetMapping("/logout")
-    public String logout(HttpSession httpSession) {
-        MemberSession memberSession = (MemberSession) httpSession.getAttribute(SessionKeys.MEMBER);
-        sessionMap.entrySet().stream()
-                .filter(entry -> entry.getValue().getNickName().equals(memberSession.getNickName()))
-                .map(Map.Entry::getKey)
-                .forEach(sessionMap::remove);
-        httpSession.removeAttribute(SessionKeys.MEMBER);
         return "redirect:/login";
     }
 }
