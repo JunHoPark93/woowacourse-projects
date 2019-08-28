977934cb61b03b9850289880a40d68d58fea40ff
강연욱 <fiasboc23@gmail.com>
2019-08-28 17:15:27 +0900

    [#133] refactor: 좋아요한 유저들 보여주기 프론트 requestMapping Url 변경

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -299,7 +299,7 @@ const INDEX_PAGE = (function () {
             const ddabongCountTag = message.querySelector('.ddabong-message');
 
             request
-                .get('/articles/' + articleId + '/ddabongs')
+                .get('/ddabongs/articles/' + articleId)
                 .then(response => {
                     console.log(response);
                     ddabongCountTag.innerText = response.data.count;
