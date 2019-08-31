2bf0b0c5a0452e34bcffec517b24553b07644984
wbluke <pwb0127@naver.com>
2019-08-31 15:49:27 +0900

    [#177] feat: search front-end와 back-end 연동

--- src/main/resources/static/js/search-box.js
+++ src/main/resources/static/js/search-box.js
@@ -42,7 +42,49 @@ const SEARCH_BOX = (function () {
     };
 
     const SearchService = function () {
+        const maxSizeOfNickName = 5;
+        const maxSizeOfHashtag = 5;
         const request = new Api().request;
+
+        const createMemberSearchedItem = function (nodes) {
+            const newNode = document.createElement("LI");
+            newNode.classList.add('search-result-member-item');
+
+            newNode.innerHTML = nodes;
+            return newNode;
+        };
+
+        const createHashtagSearchedItem = function (nodes) {
+            const newNode = document.createElement("LI");
+            newNode.classList.add('search-result-hashtag-item');
+
+            newNode.innerHTML = nodes;
+            return newNode;
+        };
+
+        const searchedMemberTemplate = function (json) {
+            return `
+            <a href="/members/${json.nickName}" class="no-pdd">
+                <div class="pdd-vertical-10 pdd-horizon-20 mrg-vertical-5">
+                    <img class="thumb-img img-circle" src="${json.profileImage}" alt="">
+                    <div class="info">
+                            <span class="title lh-1-5">${json.nickName}</span>
+                        <span class="sub-title">
+                            <span>${json.name}</span>
+                        </span>
+                    </div>
+                </div>
+            </a>`
+        };
+
+        const searchedHashtagTemplate = function (json) {
+            return `
+			<a href="/tags/${json.keyword}" class="text-dark no-pdd">
+                <div class="pdd-vertical-10 pdd-horizon-20 mrg-vertical-5">
+			        # ${json.keyword}
+			    </div>
+			</a>`
+        };
 
         const toggleSearchInput = function (event) {
             event.preventDefault();
@@ -51,22 +93,48 @@ const SEARCH_BOX = (function () {
             document.querySelector(".search-input input").focus()
         };
 
+        const removeAllChild = function (node) {
+            while (node.hasChildNodes()) {
+                node.removeChild(node.firstChild);
+            }
+        };
+
         const showSearchedList = function (event) {
-            const keyword = event.target.value;
+            const target = event.target;
+            const keyword = target.value;
+
+            const searchedList = target.closest('LI').querySelector('UL');
+
+            const appendMemberSearchedItem = function (member) {
+                const memberNode = createMemberSearchedItem(searchedMemberTemplate(member));
+                searchedList.appendChild(memberNode);
+            };
+
+            const appendHashtagSearchedItem = function (hashtag) {
+                const hashtagNode = createHashtagSearchedItem(searchedHashtagTemplate(hashtag));
+                searchedList.appendChild(hashtagNode);
+            };
 
             if (keyword.length > 0) {
                 request
                     .get("/api/search/" + keyword, {
-                        "maxSizeOfNickName": 5,
-                        "maxSizeOfHashtag": 5,
+                        "maxSizeOfNickName": maxSizeOfNickName,
+                        "maxSizeOfHashtag": maxSizeOfHashtag,
                     })
                     .then(response => {
                         return response.data;
                     })
                     .then(data => {
-                        console.log(data);
-                    });
+                        removeAllChild(searchedList);
 
+                        data.memberResponses.forEach(member => {
+                            appendMemberSearchedItem(member);
+                        });
+
+                        data.hashtagResponses.forEach(hashtag => {
+                            appendHashtagSearchedItem(hashtag);
+                        });
+                    });
 
                 document.querySelector(".advanced-search").classList.add("active")
             } else {
