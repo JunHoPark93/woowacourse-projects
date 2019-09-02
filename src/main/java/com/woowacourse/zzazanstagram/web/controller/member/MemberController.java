cabba81e3361fc487a3aca05ab7b81096aee63a7
JunHoPark <pjh374968@gmail.com>
2019-09-02 17:20:53 +0900

    refactor: 회원가입, 로그인 관련 ajax call, ResponseEntity 로 변경

--- src/main/java/com/woowacourse/zzazanstagram/web/controller/member/MemberController.java
+++ src/main/java/com/woowacourse/zzazanstagram/web/controller/member/MemberController.java
@@ -1,15 +1,17 @@
 package com.woowacourse.zzazanstagram.web.controller.member;
 
 import com.woowacourse.zzazanstagram.model.member.dto.MemberSignUpRequest;
-import com.woowacourse.zzazanstagram.model.member.exception.MemberEmailFormatException;
-import com.woowacourse.zzazanstagram.model.member.exception.MemberProfileUrlFormatException;
 import com.woowacourse.zzazanstagram.model.member.service.MemberService;
+import com.woowacourse.zzazanstagram.web.message.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.Objects;
 
 @Controller
 public class MemberController {
@@ -25,15 +27,12 @@ public class MemberController {
     }
 
     @PostMapping("/members")
-    public String saveMember(@Valid MemberSignUpRequest memberSignupRequest, BindingResult bindingResult) {
-        if (bindingResult.hasFieldErrors("email")) {
-            throw new MemberEmailFormatException(bindingResult.getFieldError().getDefaultMessage());
-        }
-        if (bindingResult.hasFieldErrors("profile")) {
-            throw new MemberProfileUrlFormatException(bindingResult.getFieldError().getDefaultMessage());
+    public ResponseEntity<ApiResponse> saveMember(@Valid MemberSignUpRequest memberSignupRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(new ApiResponse(HttpStatus.BAD_REQUEST, Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage()), HttpStatus.BAD_REQUEST);
         }
 
         memberService.save(memberSignupRequest);
-        return "redirect:/login";
+        return new ResponseEntity<>(new ApiResponse(HttpStatus.OK, "회원가입 성공!"), HttpStatus.OK);
     }
 }
