24e625cf2cf10d7cc6842ddf94bae547c6425d00
ike <fiasboc23@gmail.com>
2019-09-02 15:09:27 +0900

    [#223] refactor: 문자열 리터럴 적용하기

--- src/main/resources/static/js/ArticleCardTemplate.js
+++ src/main/resources/static/js/ArticleCardTemplate.js
@@ -99,13 +99,12 @@ const ArticleCardTemplate = function () {
         const elapsed = Math.floor((currentDate.getTime() - postedDate.getTime()) / 1000);
 
         if (elapsed >= 24 * 60 * 60) {
-            return String(postedDate.getUTCFullYear() + '년 ' + (postedDate.getMonth() + 1) + '월 ' + postedDate.getDate() + '일');
-        }
-        else if (elapsed <= 60 * 60) {
-            return String(Math.floor(elapsed / 60) + '분 전');
+            return `${postedDate.getUTCFullYear()}년${postedDate.getMonth() + 1}월${postedDate.getDate()}일`;
+        } else if (elapsed <= 60 * 60) {
+            return `${Math.floor(elapsed / 60)}분 전`;
         }
 
-        return String(Math.floor(elapsed / (60 * 60)) + '시간 전');
+        return `${Math.floor(elapsed / (60 * 60))}시간 전`;
     };
 
     const comment = function (json) {
