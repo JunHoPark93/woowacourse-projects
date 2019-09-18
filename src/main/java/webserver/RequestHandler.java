95362797ac8afbdd4be445c0a923130df4882156
JunHoPark <pjh374968@gmail.com>
2019-09-18 13:30:40 +0900

    feat: resource file output 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,15 @@
 package webserver;
 
-import java.io.*;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-
 import model.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
 import utils.RequestHeaderParser;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,18 +25,23 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            // request header 파싱해서 index 인지 검사
-            HttpRequest httpRequest = RequestHeaderParser.parseRequest(new InputStreamReader(in, StandardCharsets.UTF_8));
-
-            // TODO index.html 내려주기
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
+            String requestTarget = getRequestedTarget(in);
+            handleOutputStream(out, requestTarget);
+        } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private void handleOutputStream(OutputStream out, String requestTarget) throws IOException, URISyntaxException {
+        DataOutputStream dos = new DataOutputStream(out);
+        byte[] body = FileIoUtils.loadFileFromClasspath("./templates/" + requestTarget);
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+
+    private String getRequestedTarget(InputStream in) throws IOException {
+        HttpRequest httpRequest = RequestHeaderParser.parseRequest(new InputStreamReader(in, StandardCharsets.UTF_8));
+        return httpRequest.getRequestTarget();
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
