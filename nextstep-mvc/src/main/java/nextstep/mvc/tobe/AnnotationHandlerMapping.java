6559f6b73247c049e654189e0d255474b081a371
포비 <pobi@woowahan.com>
2019-09-25 11:39:03 +0900

    remove getHandler() implement

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -20,8 +20,6 @@ public class AnnotationHandlerMapping {
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
-        String requestUri = request.getRequestURI();
-        RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
-        return handlerExecutions.get(new HandlerKey(requestUri, rm));
+        return null;
     }
 }
