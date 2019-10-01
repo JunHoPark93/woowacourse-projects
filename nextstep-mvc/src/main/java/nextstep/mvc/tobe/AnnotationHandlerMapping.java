d1715703360e26ee5816adc39a0d67c928282edf
JunHoPark <pjh374968@gmail.com>
2019-10-01 15:16:50 +0900

    feat: 애노테이션 기반 MVC 프레임워크

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,15 @@
 package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,10 +21,38 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Set<Class<?>> annotatedWithController = findController();
 
+        for (Class<?> clazz : annotatedWithController) {
+            Method[] methods = clazz.getDeclaredMethods();
+            checkMethod(clazz, methods);
+        }
+    }
+
+    private Set<Class<?>> findController() {
+        Reflections reflections = new Reflections(basePackage);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private void checkMethod(Class<?> clazz, Method[] methods) {
+        for (Method method : methods) {
+            checkRequestMapping(clazz, method);
+        }
+    }
+
+    private void checkRequestMapping(Class<?> clazz, Method method) {
+        if (method.isAnnotationPresent(RequestMapping.class)) {
+            handlerExecutions.put(createKey(method), new HandlerExecution(clazz, method));
+        }
+    }
+
+    private HandlerKey createKey(Method method) {
+        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+        return new HandlerKey(annotation.value(), annotation.method());
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/HandlerExecution.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/HandlerExecution.java
@@ -2,9 +2,19 @@ package nextstep.mvc.tobe;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
+    private Class<?> clazz;
+    private Method method;
+
+    public HandlerExecution(Class<?> aClass, Method method) {
+        this.clazz = aClass;
+        this.method = method;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        method.invoke(clazz.newInstance(), request, response);
         return null;
     }
 }
