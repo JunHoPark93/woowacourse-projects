284ccdfbfe6bbecc09fb63ffff80f1986102ae20
JunHoPark <pjh374968@gmail.com>
2019-10-15 13:54:19 +0900

    refactor: UserDao 중복제거

--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -11,108 +11,117 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
+    private static final String INSERT_QUERY = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+    private static final String UPDATE_QUERY = "UPDATE USERS SET password = ?, name = ?, email = ? where userId = ?";
+    private static final String SELECT_ALL_QUERY = "SELECT userId, password, name, email FROM USERS";
+    private static final String SELECT_QUERY = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+
     private Connection con;
     private PreparedStatement pstmt;
-    private ResultSet rs;
 
     public void insert(User user) throws SQLException {
         try {
             con = ConnectionManager.getConnection();
-            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, user.getUserId());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getName());
-            pstmt.setString(4, user.getEmail());
-
+            pstmt = con.prepareStatement(INSERT_QUERY);
+            setInsertParameters(user);
             pstmt.executeUpdate();
         } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
+            closeResources();
         }
     }
 
     public void update(User user) throws SQLException {
         try {
             con = ConnectionManager.getConnection();
-            String sql = "UPDATE USERS SET password = ?, name = ?, email = ? where userId = ?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, user.getPassword());
-            pstmt.setString(2, user.getName());
-            pstmt.setString(3, user.getEmail());
-            pstmt.setString(4, user.getUserId());
-
+            pstmt = con.prepareStatement(UPDATE_QUERY);
+            setUpdateParameters(user);
             pstmt.executeUpdate();
         } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
+            closeResources();
         }
     }
 
     public List<User> findAll() throws SQLException {
-        List<User> users = new ArrayList<>();
-
         try {
             con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS";
-            pstmt = con.prepareStatement(sql);
-
+            pstmt = con.prepareStatement(SELECT_ALL_QUERY);
             ResultSet resultSet = pstmt.executeQuery();
-            while (resultSet.next()) {
-                String userId = resultSet.getString("userId");
-                String password = resultSet.getString("password");
-                String name = resultSet.getString("name");
-                String email = resultSet.getString("email");
-                users.add(new User(userId, password, name, email));
-            }
-            return users;
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
 
-            if (con != null) {
-                con.close();
-            }
+            return getUsers(resultSet);
+        } finally {
+            closeResources();
         }
     }
 
     public User findByUserId(String userId) throws SQLException {
+        ResultSet rs = null;
         try {
             con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-            pstmt = con.prepareStatement(sql);
+            pstmt = con.prepareStatement(SELECT_QUERY);
             pstmt.setString(1, userId);
 
             rs = pstmt.executeQuery();
 
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-            }
+            return getUser(rs);
 
-            return user;
         } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
+            closeResources(rs);
         }
+    }
+
+    private void setUpdateParameters(User user) throws SQLException {
+        pstmt.setString(1, user.getPassword());
+        pstmt.setString(2, user.getName());
+        pstmt.setString(3, user.getEmail());
+        pstmt.setString(4, user.getUserId());
+    }
+
+    private List<User> getUsers(ResultSet resultSet) throws SQLException {
+        List<User> users = new ArrayList<>();
+        while (resultSet.next()) {
+            String userId = resultSet.getString("userId");
+            String password = resultSet.getString("password");
+            String name = resultSet.getString("name");
+            String email = resultSet.getString("email");
+            users.add(new User(userId, password, name, email));
+        }
+        return users;
+    }
+
+    private User getUser(ResultSet rs) throws SQLException {
+        User user = null;
+        if (rs.next()) {
+            user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                    rs.getString("email"));
+        }
+        return user;
+    }
+
+    private void closeResources() throws SQLException {
+        if (pstmt != null) {
+            pstmt.close();
+        }
+        if (con != null) {
+            con.close();
+        }
+    }
+
+    private void closeResources(ResultSet rs) throws SQLException {
+        if (rs != null) {
+            rs.close();
+        }
+        if (pstmt != null) {
+            pstmt.close();
+        }
+        if (con != null) {
+            con.close();
+        }
+    }
+
+    private void setInsertParameters(User user) throws SQLException {
+        pstmt.setString(1, user.getUserId());
+        pstmt.setString(2, user.getPassword());
+        pstmt.setString(3, user.getName());
+        pstmt.setString(4, user.getEmail());
     }
 }
