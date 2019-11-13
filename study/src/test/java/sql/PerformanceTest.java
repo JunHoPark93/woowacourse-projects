048e0bf03650b07fab3b8485ff921d9e57920797
JunHoPark <pjh374968@gmail.com>
2019-11-13 13:15:52 +0900

    refactor: query 문 따로 분리

--- study/src/test/java/sql/PerformanceTest.java
+++ study/src/test/java/sql/PerformanceTest.java
@@ -12,23 +12,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/***
- * use jwp_jdbc;
- * CREATE VIEW dev_type AS
- *     SELECT
- *         SUBSTRING_INDEX(SUBSTRING_INDEX(DevType, ';', numbers.n), ';', - 1) AS dev_type,
- *         REPLACE(LEFT(YearsCodingProf, 2), '-', '') AS period
- *     FROM
- *     (SELECT 1 AS n UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5
- *     UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9 UNION ALL SELECT 10
- *     UNION ALL SELECT 11 UNION ALL SELECT 12 UNION ALL SELECT 13 UNION ALL SELECT 14 UNION ALL SELECT 15
- *     UNION ALL SELECT 16 UNION ALL SELECT 17 UNION ALL SELECT 18 UNION ALL SELECT 19 UNION ALL SELECT 20) AS
- *         numbers
- *             INNER JOIN
- *         survey_results_public ON CHAR_LENGTH(DevType) - CHAR_LENGTH(REPLACE(DevType, ';', '')) >= numbers.n - 1
- *     WHERE
- *         NOT YearsCodingProf = 'NA';
- */
 public class PerformanceTest {
     private JdbcTemplate jdbcTemplate = JdbcTemplate.builder().url("jdbc:mysql://localhost:13306/jwp_jdbc?useUnicode=true&characterEncoding=utf8")
             .driver("com.mysql.cj.jdbc.Driver")
